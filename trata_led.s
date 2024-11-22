b9bd1fce3ce72c0e051a082b6f03dbe5af15be79
Vitor Ferreira <112410843+Vitor-Ferreira005@users.noreply.github.com>
2024-11-21 22:42:20 -0300

    Update trata_led.s

--- trata_led.s
+++ trata_led.s
@@ -12,13 +12,31 @@ ACENDE_LED:
     subi    r14,r14,0x30
     muli    r14,r14,0xA
     subi    r19,r19,0x30
+    add     r14,r14,r19 /*r14 eh o offset para o sll*/
+    movi    r19,0x1
+    sll     r14,r19,r14 /*para obter n atual que deve acender*/
+
+    ldw     r19, 0(r18)
+    or      r14,r14,r19  /*LEDs anteriores ficam acesos*/
+
+    stwio   r14,0(r18)
+    ret
+APAGA_LED:
+    movia   r18, 0x10000000
+    ldw     r14, 12(r10) /*dezena*/
+    ldw     r19, 16(r10) /*unidade*/
+    subi    r14,r14,0x30
+    muli    r14,r14,0xA
+    subi    r19,r19,0x30
     add     r14,r14,r19
     movi    r19,0x1
-    sll     r14,r19,r14
-    or      r14,r14,r18
-    stwio   r14,0(r18)
-APAGA_LED:
+    sll     r14,r19,r14 
+    nor     r14,r14,r14 /*para obter n atual que deve apagar*/
+    
+    ldw     r19, 0(r18)
+    and     r14,r14,r19  /*LEDs anteriores*/
 
+    stwio   r14,0(r18)
     ret
 .end
 
